9502cfad7e0e23ae8df528d9d1b3a24553d348a8
Melissa Yu <themelissayu@gmail.com>
2015-11-28 18:33:24 -0500

    added star ratings, migrated db

--- cs50_final/cs50_final/settings.py
+++ cs50_final/cs50_final/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'search',
+    'star_ratings'
 )
 
 MIDDLEWARE_CLASSES = (
--- cs50_final/search/models.py
+++ cs50_final/search/models.py
@@ -2,16 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class User(authmodels.User):
-    zip_code = lfmodels.USZipCodeField()
-    prof_pic = models.ImageField(upload_to=rename_file, max_length=300)
+class Club(models.Model):
+	name = models.CharField(max_length=30)
+	genre = models.CharField(max_length=30)
+	description = models.TextField(max_length=2000)
+	# ratings?
 
-class Club(models.Model)
-    name = models.CharField(max_length=30)
-    description = 
-    genre =
-    rating = 
-    
-class Reviews(models.Model)
-    name = models.CharField(max_length=30)
-    reviews =
+class Reviews(models.Model):
+	name = models.ForeignKey(Club)
+	review = models.TextField(max_length=2000)
