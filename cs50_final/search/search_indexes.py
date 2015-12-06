0c59f0e8c3cb3036d3f63aef2fb5ee55aea084e5
Melissa Yu <themelissayu@gmail.com>
2015-12-06 04:53:59 -0500

    Commented code, deleted old files

--- cs50_final/search/search_indexes.py
+++ cs50_final/search/search_indexes.py
@@ -1,15 +1,10 @@
 from haystack import indexes
 from search.models import Club
 
-
+# haystack index for Club model
 class ClubIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document=True, use_template=True)
 	abbreviation = indexes.CharField(model_attr='abbreviation')
-	# genre = indexes.CharField(model_attr='genre')
 
 	def get_model(self):
 		return Club
-
-	# def index_queryset(self, using=None):
-	#     """Used when the entire index for model is updated."""
-	#     return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
